719dec444d13195b7276a4007b788fe927cecc03
TerenceCooper <TerenceCooper@tutamail.com>
2015-12-01 10:00:46 +0800

    Before refractoring the functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -36,22 +36,37 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
-
+        import time
+        time.sleep(5)
         table = self.browser.find_element_by_id('id_list_table')
-        rows = find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows)
-        )
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        
+        #self.assertTrue(
+        #    any(row.text == '1:Buy peacock feathers' for row in rows),
+        #    "New to-do item did not appear in table"
+        #)
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
-        
+        input_box = self.browser.find_element_by_id('id_new_item')        
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(3)        
+
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly' ,
+            [row.text for row in rows]
+        )
 
         # Edith wonders wether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- ther is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
